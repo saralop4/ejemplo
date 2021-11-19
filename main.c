a61299350908d9c9ca1d22aa85409c9a7dde8dbb
saralop4 <saralopezjb16@gmail.com>
2021-11-19 14:08:40 -0500

    arreglamos el issue

--- main.c
+++ main.c
@@ -1,4 +1,12 @@
 int main(void){
 	
 	printf("Hola Mundo");
+
+	// git clone <repo>
+	//git fetch
+	//pull origin main
+	// git commit -am "mensaje"
+	//git push origin main
+	printf("Cerramos la ventana");
+
 }
